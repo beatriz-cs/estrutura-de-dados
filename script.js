7f9d53fbed03551914b98062b01c12f709e7d07d
beatriz-cs <beatrizcardeall@outlook.com>
2022-06-22 23:05:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// Crie uma variável que receba um nome;
+
+let nome = "Beatriz"
+
+console.log(nome);
+
+// Crie uma variável que receba um número;
+
+let numero = 23
+
+console.log(numero);
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+
+let ano = 2022
+
+console.log(ano - numero);
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+
+console.log(numero + ano);
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+
+console.log("Estou mexendo no console.log");
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+
+let quartaFeira = true
+
+console.log(quartaFeira);
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+
+console.log(typeof quartaFeira)
